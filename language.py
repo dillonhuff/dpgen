b6830f7615f33772fc1b8f0d941b425d01ffbf40
Dillon <dillon@DN0a226992.SUNet>
2021-10-02 20:11:34 -0700

    adding case expressions

--- language.py
+++ language.py
@@ -6,9 +6,11 @@ class ASTNode:
 
     def __repr__(self):
         if self.tag == 'call':
-            return '({0} {1})'.format(self.args[0], self.args[1:])
+            return '({0} {1})'.format(self.args[0], ' '.join(map(str, self.args[1:])))
         elif self.tag == 'lam':
-            return '(lam {0}. {1})'.format(self.args[0], self.args[1:])
+            return '(lam {0}. {1})'.format(self.args[0], ' '.join(map(str, self.args[1:])))
+        elif self.tag == 'case':
+            return '(case {0})'.format(' '.join(map(str, self.args)))
         else:
             print('Tag:', self.tag)
             assert(len(self.args) == 0)
@@ -23,8 +25,12 @@ def lam(var, expr):
 def call(fun, args):
     return ASTNode('call', [fun] + args)
 
+def case(cases):
+    return ASTNode('case', cases)
+
 ssa = call(prim('ss'), [prim('a')])
-obj = lam(prim('s'), call('F', [prim('s')]))
+F = case([call(prim('='), [call(prim('len'), [prim('s')]), prim(0)])])
+obj = lam(prim('s'), call(F, [prim('s')]))
 nd = call(prim('max'), [ssa, obj])
 
 print(nd)
