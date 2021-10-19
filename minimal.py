ceb01305b104b6539048673a73bf10509cdb5666
Dillon <dillon@DN0a2261b6.SUNet>
2021-10-18 20:12:01 -0700

    Adding special cases

--- minimal.py
+++ minimal.py
@@ -1,33 +1,66 @@
-def print_dp(M):
-    surrounding = ''
-    for i in range(M + 1):
-        surrounding += 'case |a| = ' + str(i) + ' ->\n'
-        surrounding += '  return B {}'.format(i) + '\n'
+import os
 
-    surrounding += 'case |a| > ' + str(M) + ' ->\n'
-    surrounding += '  mx = -\u221e\n'
+def run_cmd(cmd):
+    res = os.system(cmd)
+    assert(res == 0)
+
+def print_dp(dpspec, filename, test_cases):
+    M = dpspec.M()
+
+    surrounding = ''
+    for bf in dpspec.base_cases:
+        surrounding += bf + '\n'
+
+    surrounding += 'NEG_INF = -1000000000\n\n'
+
+    surrounding += 'def driver(a):\n'
+    for i in range(M + 1):
+        surrounding += '  if len(a) == ' + str(i) + ':\n'
+        surrounding += '    return B_{}(a)'.format(i) + '\n'
+
+    surrounding += '  if len(a) > ' + str(M) + ':\n'
+    surrounding += '    mx = NEG_INF\n'
     dpvars = ''
     vs = ''
     for i in range(M):
-        surrounding += '  ' + ('  ' * i) + 'for i{} in []:\n'.format(i)
+        surrounding += '    ' + ('  ' * i) + 'for i{} in []:\n'.format(i)
         dpvars += 'a[i{}]'.format(i)
         vs += 'v{}'.format(i)
 
     recargs = 'v[e]'
     for i in range(0, M - 1):
         recargs += ' v{}'.format(i)
-    surrounding += '  ' + ('  ' * M) + 'mx = max(mx, DP a[:i{0}) {1})'.format(M - 1, dpvars) + '\n'
-    surrounding += '  return mx\n'
+    surrounding += '    ' + ('  ' * M) + 'mx = max(mx, DP(a[:i{0} - 1], {1}))'.format(M - 1, dpvars) + '\n'
+    surrounding += '    return mx\n'
     surrounding += '\n'
-    surrounding += 'def DP v {0}'.format(vs) + ' =\n'
+    surrounding += 'def DP(v, {0}):'.format(vs) + '\n'
     aargs = []
     for i in range(0, M):
         aargs.append('v[{}]'.format(i))
     aargs.append('v0')
-    surrounding += '  case |v| = ' + str(M) + ' ->\n'
+    surrounding += '  if len(v) == ' + str(M) + ':\n'
     surrounding += '    return f({})\n'.format(', '.join(aargs))
-    surrounding += '  case |v| > 1 ->\n'
-    surrounding += '    max [1, |v| - 1] (\u03bb e. (DP v[:e) {1}) + f(v[e], {2}))'.format(vs, recargs, vs) + '\n\n'
-    print(surrounding)
+    surrounding += '  if len(v) > 1:\n'
+    surrounding += '    mx = NEG_INF\n'
+    surrounding += '    for e in range(1, len(v)):\n'
+    surrounding += '      mx = max(mx, DP(v[:e - 1], {1}) + f(v[e], {2}))'.format(vs, recargs, vs) + '\n\n'
+    surrounding += '    return mx\n'
 
-print_dp(1)
+    surrounding += '\n\n'
+    for case in test_cases:
+        surrounding += 'assert(driver({0}) == {1})\n'.format(case[0], case[1])
+    open(filename, 'w').write(surrounding) # rint(surrounding)
+
+class DPSpec:
+
+    def __init__(self, base_cases, f):
+        self.base_cases = base_cases
+        self.f = f
+
+    def M(self):
+        return len(self.base_cases) - 1
+
+test_cases = [([], 0)]
+print_dp(DPSpec(['def B_0(a): return 0', 'def B_1(a): return 0'], 'math.abs(v0 - v1)'), 'dp.py', test_cases)
+run_cmd('python dp.py')
+
