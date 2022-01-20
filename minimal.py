efd17a099f7bb7cf2aa36b1a2383052d1767a1d8
Dillon Huff <dillonhuff@gmail.com>
2022-01-19 18:30:24 -0800

    Adding an example of constrained subsequence sums

--- minimal.py
+++ minimal.py
@@ -14,11 +14,11 @@ def print_dp(dpspec, filename, test_cases):
         surrounding += bf + '\n'
     surrounding += dpspec.f + '\n\n'
 
-    paramstr = ', '.join(dpspec.parameters)
+    paramstr = '' if len(dpspec.parameters) == 0 else ', ' + ', '.join(dpspec.parameters)
 
     surrounding += 'class Solution(object):\n'
 
-    surrounding += '  def {0}(self, a, {1}):\n'.format(dpspec.name, paramstr)
+    surrounding += '  def {0}(self, a {1}):\n'.format(dpspec.name, paramstr)
     for i in range(M + 1):
         surrounding += '    if len(a) == ' + str(i) + ':\n'
         surrounding += '      return B_{}(a)'.format(i) + '\n'
@@ -36,11 +36,11 @@ def print_dp(dpspec, filename, test_cases):
     recargs = 'e'
     for i in range(0, M - 1):
         recargs += ' v{}'.format(i)
-    surrounding += '      ' + ('  ' * M) + 'mx = max(mx, self.DP(a, {1}, memo))'.format(M - 1, dpvars) + '\n'
+    surrounding += '      ' + ('  ' * M) + 'mx = max(mx, self.DP(a, {1}, memo {2}))'.format(M - 1, dpvars, paramstr) + '\n'
     surrounding += '      return mx\n'
     surrounding += '\n'
 
-    surrounding += '  def DP(self, a, {0}, memo):'.format(vs) + '\n'
+    surrounding += '  def DP(self, a, {0}, memo {1}):'.format(vs, paramstr) + '\n'
     surrounding += '    if {0} in memo:\n'.format(vs)
     surrounding += '      return memo[{0}]\n'.format(vs)
     surrounding += '    v = {0} - 1\n'.format(vs)
@@ -55,7 +55,7 @@ def print_dp(dpspec, filename, test_cases):
     surrounding += '      for e in range(v + 1):\n'
     surrounding += '        mx = max(mx, B_0([]) + B_1([e]) + f(a, e, v0))\n'
     surrounding += '      for e in range(1, v + 1):\n'
-    surrounding += '        mx = max(mx, self.DP(a, {1}, memo) + f(a, e, {2}))'.format(vs, recargs, vs) + '\n\n'
+    surrounding += '        mx = max(mx, self.DP(a, {1}, memo {3}) + f(a, e, {2}))'.format(vs, recargs, vs, paramstr) + '\n\n'
     surrounding += '      memo[v0] = mx\n'
     surrounding += '      return mx\n'
 
@@ -88,7 +88,7 @@ run_cmd('python lengthOfLIS.py')
 name = 'constrainedSubsetSum'
 test_cases = []
 lis_base_cases = ['def B_0(a): return 0', 'def B_1(a): return 1']
-lis = DPSpec(name, lis_base_cases, 'def f(a, v0, v1): return 1 if a[v0] < a[v1] else NEG_INF', ['k'])
+lis = DPSpec(name, lis_base_cases, 'def f(a, v0, v1, k): return 1 if a[v0] < a[v1] else NEG_INF', ['k'])
 print_dp(lis, name + '.py', test_cases)
 run_cmd('python ' + name + '.py')
 
