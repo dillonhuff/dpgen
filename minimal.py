db4697fc10a95fdd0f4bcdffe9d5f14be5f399a3
Dillon Huff <dillonhuff@gmail.com>
2022-01-24 19:04:04 -0800

    Making all base case functions take the whole input array and indexes as arguments

--- minimal.py
+++ minimal.py
@@ -19,9 +19,10 @@ def print_dp(dpspec, filename, test_cases):
     surrounding += 'class Solution(object):\n'
 
     surrounding += '  def {0}(self, a {1}):\n'.format(dpspec.name, paramstr)
-    for i in range(M + 1):
-        surrounding += '    if len(a) == ' + str(i) + ':\n'
-        surrounding += '      return B_{}(a)'.format(i) + '\n'
+    surrounding += '    if len(a) == ' + str(0) + ':\n'
+    surrounding += '      return B_{}(a)'.format(0) + '\n'
+    surrounding += '    if len(a) == ' + str(1) + ':\n'
+    surrounding += '      return B_{}(a, 0)'.format(1) + '\n'
 
     surrounding += '    if len(a) > ' + str(M) + ':\n'
     surrounding += '      mx = NEG_INF\n'
@@ -49,13 +50,13 @@ def print_dp(dpspec, filename, test_cases):
         aargs.append('{}'.format(i))
     aargs.append('v0')
     surrounding += '    if v + 1 == ' + str(M) + ':\n'
-    surrounding += '      return max(B_0([]) + B_1([v0]), B_0([]) + B_1(v) + f(a, v, v0))\n'
+    surrounding += '      return max(B_0([]) + B_1(a, v0), B_0([]) + B_1(a, v) + {})\n'.format(dpspec.callf('v', 'v0'))
     surrounding += '    if v + 1 > 1:\n'
     surrounding += '      mx = 0\n'
     surrounding += '      for e in range(v + 1):\n'
-    surrounding += '        mx = max(mx, B_0([]) + B_1([e]) + f(a, e, v0))\n'
+    surrounding += '        mx = max(mx, B_0([]) + B_1(a, e) + {})\n'.format(dpspec.callf('e', 'v0'))
     surrounding += '      for e in range(1, v + 1):\n'
-    surrounding += '        mx = max(mx, self.DP(a, {1}, memo {3}) + f(a, e, {2}))'.format(vs, recargs, vs, paramstr) + '\n\n'
+    surrounding += '        mx = max(mx, self.DP(a, {1}, memo {3}) + {4})'.format(vs, recargs, vs, paramstr, dpspec.callf('e', vs)) + '\n\n'
     surrounding += '      memo[v0] = mx\n'
     surrounding += '      return mx\n'
 
@@ -83,18 +84,19 @@ class DPSpec:
         return 'f(a, {0}, {1} {2})'.format(a, b, paramstr)
 
 test_cases = [([], 0), ([1], 0), ([0, 200], 200)]
-print_dp(DPSpec('maxAbs', ['def B_0(a): return 0', 'def B_1(a): return 0'], 'def f(a, v0, v1): return abs(a[v0] - a[v1])'), 'dp.py', test_cases)
+print_dp(DPSpec('maxAbs', ['def B_0(a): return 0', 'def B_1(a, e): return 0'], 'def f(a, v0, v1): return abs(a[v0] - a[v1])'), 'dp.py', test_cases)
 run_cmd('python dp.py')
 
 test_cases = [([1, 0], 1), ([-1, 2], 2), ([10,9,2,5,3,7,101,18], 4), ([2,15,3,7,8,6,18], 5)]
-lis_base_cases = ['def B_0(a): return 0', 'def B_1(a): return 1']
+lis_base_cases = ['def B_0(a): return 0', 'def B_1(a, e): return 1']
 lis = DPSpec('lengthOfLIS', lis_base_cases, 'def f(a, v0, v1): return 1 if a[v0] < a[v1] else NEG_INF')
 print_dp(lis, 'lengthOfLIS.py', test_cases)
 run_cmd('python lengthOfLIS.py')
 
 name = 'constrainedSubsetSum'
-test_cases = [(([1], 1), 1)]
-lis_base_cases = ['def B_0(a): return 0', 'def B_1(a): return 1']
+test_cases = [(([1], 1), 1), (([10,2,-10,5,20], 2), 37)]
+
+lis_base_cases = ['def B_0(a): return 0', 'def B_1(a, e): return a[e]']
 lis = DPSpec(name, lis_base_cases, 'def f(a, v0, v1, k): return 1 if a[v0] < a[v1] else NEG_INF', ['k'])
 print_dp(lis, name + '.py', test_cases)
 run_cmd('python ' + name + '.py')
