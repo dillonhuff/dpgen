42f7ef4848a643fc37a6107da34c1cd8c8ae0b34
Dillon Huff <dillonhuff@gmail.com>
2022-01-29 20:42:08 -0800

    Changed B_ to M_

--- minimal.py
+++ minimal.py
@@ -20,15 +20,15 @@ def print_dp(dpspec, filename, test_cases):
 
     surrounding += '  def {0}(self, a {1}):\n'.format(dpspec.name, paramstr)
     surrounding += '    if len(a) == ' + str(0) + ':\n'
-    surrounding += '      return B_{}(a)'.format(0) + '\n'
+    surrounding += '      return A_{}(a)'.format(0) + '\n'
     surrounding += '    if len(a) == ' + str(1) + ':\n'
-    surrounding += '      return B_{}(a, 0)'.format(1) + '\n'
+    surrounding += '      return A_{}(a, 0)'.format(1) + '\n'
 
     surrounding += '    if len(a) > ' + str(M) + ':\n'
     surrounding += '      mx = NEG_INF\n'
     surrounding += '      memo = {}\n'
     surrounding += '      for i in range(len(a)):\n'
-    surrounding += '        mx = max(mx, B_0(a) + B_1(a, i))\n'
+    surrounding += '        mx = max(mx, A_0(a) + A_1(a, i))\n'
     dpvars = ''
     vs = ''
     surrounding += '      ' + ('  ' * 0) + 'for i{} in range(1, len(a)):\n'.format(0)
@@ -48,11 +48,11 @@ def print_dp(dpspec, filename, test_cases):
     aargs.append('{}'.format(0))
     aargs.append('v0')
     surrounding += '    if v0 == ' + str(M) + ':\n'
-    surrounding += '      return B_0([]) + B_1(a, v0 - 1) + {}\n'.format(dpspec.callf('v0 - 1', 'v0'))
+    surrounding += '      return A_0([]) + A_1(a, v0 - 1) + {}\n'.format(dpspec.callf('v0 - 1', 'v0'))
     surrounding += '    if v0 > 1:\n'
     surrounding += '      mx = NEG_INF\n'
     surrounding += '      for e in range(v0):\n'
-    surrounding += '        mx = max(mx, B_0([]) + B_1(a, e) + {})\n'.format(dpspec.callf('e', 'v0'))
+    surrounding += '        mx = max(mx, A_0([]) + A_1(a, e) + {})\n'.format(dpspec.callf('e', 'v0'))
     surrounding += '      for e in range(1, v0):\n'
     surrounding += '        mx = max(mx, {4} + self.DP(a, {1}, memo {3}))'.format(vs, recargs, vs, paramstr, dpspec.callf('e', vs)) + '\n\n'
     surrounding += '      memo[v0] = mx\n'
@@ -79,23 +79,23 @@ class DPSpec:
 
     def callf(self, a, b):
         paramstr = '' if len(self.parameters) == 0 else ', ' + ', '.join(self.parameters)
-        return 'f(a, {0}, {1} {2})'.format(a, b, paramstr)
+        return 'M(a, {0}, {1} {2})'.format(a, b, paramstr)
 
 test_cases = [([], 0), ([1], 0), ([0, 200], 200)]
-print_dp(DPSpec('maxAbs', ['def B_0(a): return 0', 'def B_1(a, e): return 0'], 'def f(a, v0, v1): return abs(a[v0] - a[v1])'), 'dp.py', test_cases)
+print_dp(DPSpec('maxAbs', ['def A_0(a): return 0', 'def A_1(a, e): return 0'], 'def M(a, v0, v1): return abs(a[v0] - a[v1])'), 'dp.py', test_cases)
 run_cmd('python dp.py')
 
 test_cases = [([1, 0], 1), ([-1, 2], 2), ([10,9,2,5,3,7,101,18], 4), ([2,15,3,7,8,6,18], 5)]
-lis_base_cases = ['def B_0(a): return 0', 'def B_1(a, e): return 1']
-lis = DPSpec('lengthOfLIS', lis_base_cases, 'def f(a, v0, v1): return 1 if a[v0] < a[v1] else NEG_INF')
+lis_base_cases = ['def A_0(a): return 0', 'def A_1(a, e): return 1']
+lis = DPSpec('lengthOfLIS', lis_base_cases, 'def M(a, v0, v1): return 1 if a[v0] < a[v1] else NEG_INF')
 print_dp(lis, 'lengthOfLIS.py', test_cases)
 run_cmd('python lengthOfLIS.py')
 
 name = 'constrainedSubsetSum'
 test_cases = [(([1], 1), 1), (([10,2,-10,5,20], 2), 37), (([10,2], 2), 12) , (([-1,-2,-3], 1), -1)]
 
-lis_base_cases = ['def B_0(a): return 0', 'def B_1(a, e): return a[e]']
-lis = DPSpec(name, lis_base_cases, 'def f(a, v0, v1, k): return a[v1] if v1 - v0 <= k else NEG_INF', ['k']) 
+lis_base_cases = ['def A_0(a): return 0', 'def A_1(a, e): return a[e]']
+lis = DPSpec(name, lis_base_cases, 'def M(a, v0, v1, k): return a[v1] if v1 - v0 <= k else NEG_INF', ['k']) 
 print_dp(lis, name + '.py', test_cases)
 run_cmd('python ' + name + '.py')
 
