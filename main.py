8ec946ce30bea82652ef73145d4cffb1acba39e6
Dillon Huff <dillonhuff@gmail.com>
2022-01-30 21:29:20 -0800

    removing v0 params

--- main.py
+++ main.py
@@ -76,7 +76,7 @@ def print_dp(dpspec, filename, test_cases):
         txt += '    for e in range(v0):\n'
         txt += '      mx = {1}(mx, L(a, e) + {0})\n'.format(dpspec.callM('e', 'v0'), dpspec.direction())
         txt += '    for e in range(1, v0):\n'
-        txt += '      mx = {5}(mx, {4} + self.DP(a, {1}, memo {3}))'.format('v0', 'e', 'v0', paramstr, dpspec.callM('e', 'v0'), dpspec.direction()) + '\n\n'
+        txt += '      mx = {3}(mx, {2} + self.DP(a, {0}, memo {1}))'.format('e', paramstr, dpspec.callM('e', 'v0'), dpspec.direction()) + '\n\n'
     txt += '    memo[(v0 {})] = mx\n'.format(paramstr)
     txt += '    return mx\n'
 
